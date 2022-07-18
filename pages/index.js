2c88e5fe50e37682e52bda025bca631575c81b7e
JunnieLee <heunghari@naver.com>
2022-07-18 17:45:39 +0900

    NFT box component added with some formattings

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from "next/head"
 import styles from "../styles/Home.module.css"
+import { useMoralisQuery } from "react-moralis"
+import NFTBox from "../components/NFTBox"
 
 export default function Home() {
     // How do we show the recently listed NFTs?
@@ -11,5 +12,43 @@ export default function Home() {
 
     // TheGraph does this in a decentralized way
     // Moralis does it in a centralized way and comes with a ton of other features.
-    return <div className={styles.container}>Hi~~~</div>
+
+    const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+        // TableName
+        // Function for the query
+        "ActiveItem",
+        (query) => query.limit(10).descending("tokenId")
+    ) // grab from our database on the ActiveItem table,
+    // and just grab from the first 10 with descending order based on their tokenId
+
+    console.log(listedNfts)
+
+    return (
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <div className="flex flex-wrap">
+                {fetchingListedNfts ? (
+                    <div>Loading...</div>
+                ) : (
+                    listedNfts.map((nft) => {
+                        console.log(nft.attributes)
+                        const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                            nft.attributes
+                        return (
+                            <div key={tokenId}>
+                                <NFTBox
+                                    price={price}
+                                    nftAddress={nftAddress}
+                                    tokenId={tokenId}
+                                    marketplaceAddress={marketplaceAddress}
+                                    seller={seller}
+                                    key={`${nftAddress}${tokenId}`}
+                                />
+                            </div>
+                        )
+                    })
+                )}
+            </div>
+        </div>
+    )
 }
